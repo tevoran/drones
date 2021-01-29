ae486b0513d0b6955f79b1f2c99a812c7d913f08
tevoran <j.pfeiffer93@gmx.de>
2021-01-29 22:20:51 +0100

    added crosshair

--- src/main.c
+++ src/main.c
@@ -16,15 +16,11 @@ void main()
 	struct vec3f player_location={0,0,0};
 	gfx_create_camera(player_location, 0.5*PI);
 
-	struct vec3f drone_location={0,0,10};
+	struct vec3f drone_location={0,0,5};
 	struct model *drone=gfx_load_model("data/models/drone/drone.obj", drone_location);
-	printf("1\n");
 	gfx_model_load_texture("data/models/drone/drone.bmp", drone);
-	printf("2\n");
 
 	gfx_ui_set_font("data/ConsoleFont.bmp", 256);
-
-	gfx_ui_printf(300,300, NULL, 12, "Hallo");
 
 
 	gfx_create_skybox(
@@ -35,7 +31,7 @@ void main()
 		"data/sky.bmp",
 		"data/sky.bmp");
 
-	struct ui_window *window_test1=gfx_ui_create_window(300,300,0,0);
+	struct ui_window *crosshair=gfx_ui_create_window(4,4,engine_config_state().resolution_x/2-2,engine_config_state().resolution_y/2-2);
 
 	//main loop
 	int quit=0;
